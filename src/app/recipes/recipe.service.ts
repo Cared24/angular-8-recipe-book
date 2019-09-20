6333d982e18f0082d839ce71c51a678878b8c237
Cared24 <ponuszrichard93@gmail.com>
2019-09-20 10:10:06 +0200

    Added firebase with http functions for recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,32 +8,14 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Burger King Burger',
-      'Burger King -Burger only, nothing else matters',
-      'https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Salad', 20),
-        new Ingredient('Buns', 2)
-      ]
-    ),
-
-    new Recipe(
-      'Pizza',
-      'Best pizza in Town!',
-      'https://food-images.files.bbci.co.uk/food/recipes/quick_pepperoni_pizza_64616_16x9.jpg',
-      [
-        new Ingredient('Flour', 1),
-        new Ingredient('Potato', 3),
-        new Ingredient('Bacon', 14)
-      ]
-    )
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice(); // this return a copy of the recipes array
